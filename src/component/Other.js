9438c6ec3368695b663dfd9d40ed8cb14415dc15
aaavieri <aaavieri@gmail.com>
2019-10-16 22:49:44 +0800

    develop

--- src/component/Other.js
+++ src/component/Other.js
@@ -1,41 +1,70 @@
 import React, { useState, useEffect } from 'react';
-import { Grid, Avatar, Typography } from '@material-ui/core';
+import { Grid, Avatar, Typography, Fab, CircularProgress } from '@material-ui/core';
+import { green } from '@material-ui/core/colors';
 import {makeStyles} from '@material-ui/core/styles';
 import {connect} from "react-redux";
 import PokerCard from './PokerCard'
 import {http} from '../util';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
     container: {
         width: '100%',
         height: '100%',
         flexGrow: 1,
         padding: 20
+    },
+    wrapper: {
+        margin: theme.spacing(1),
+        position: 'relative',
+    },
+    buttonSuccess: {
+        backgroundColor: green[500],
+        '&:hover': {
+            backgroundColor: green[700],
+        },
+    },
+    fabProgress: {
+        color: green[500],
+        position: 'absolute',
+        top: -6,
+        left: -6,
+        zIndex: 1,
+    },
+    buttonProgress: {
+        color: green[500],
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        marginTop: -12,
+        marginLeft: -12,
     }
-});
+}));
 
 function Inner(props) {
     const classes = useStyles();
-    const { info: {userId}, position } = props;
+    const { info: {userId, lordUser}, position } = props;
     const [ cardLabel, setCardLabel ] = useState("");
     const [ otherUser, setOtherUser ] = useState("未加入");
     useEffect(() => {
         setCardLabel("未发牌");
     });
-    const { avatarSrc, setAvatarSrc } = useState("/asset/img/unknown.jpg");
+    const [ avatarSrc, setAvatarSrc ] = useState("/asset/img/unknown.jpg");
+    const getUserLabel = userId => {
+        return `${userId}(${userId === lordUser ? "地主" : "农民"})`
+    };
     http.addDataListener("JoinGame", ({userList}) => {
         if (userList.length === 2) {
             if (position === -1 && userId === userList[1]) {
-                setOtherUser(userList[0]);
+                setOtherUser(getUserLabel(userList[0]));
             } else if (position === 1 && userId === userList[0]) {
-                setOtherUser(userList[1]);
+                setOtherUser(getUserLabel(userList[1]));
             }
         } else if (userList.length === 3) {
             const index = userList.findIndex(userId);
             if (position === -1) {
-                setOtherUser(userList[(index + 2) % 3]);
+                setOtherUser(getUserLabel(userList[(index + 2) % 3]));
             } else if (position === 1) {
-                setOtherUser(userList[(index + 1) % 3]);
+                setOtherUser(getUserLabel(userList[(index + 1) % 3]));
             }
         }
     });
@@ -59,6 +88,15 @@ function Inner(props) {
             </Grid>
             <Grid item>
                 <PokerCard cardLabel={cardLabel}/>
+            </Grid>
+            <Grid item alignItems={"center"} justify={"center"}>
+                <div className={classes.wrapper}>
+                    <Fab aria-label="save"
+                         color="primary"
+                         className={classes.buttonSuccess}>
+                    </Fab>
+                    <CircularProgress size={68} className={classes.fabProgress} />
+                </div>
             </Grid>
         </Grid>
     );
